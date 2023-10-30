e35e7b5dcb812198341b9ad24adc64d207c01703
Ssuamje <side2850@naver.com>
2023-10-30 19:24:01 +0900

    FIX

--- src/main/java/racingcar/game/RacingGameStrategyFactory.java
+++ src/main/java/racingcar/game/RacingGameStrategyFactory.java
@@ -7,9 +7,11 @@ public class RacingGameStrategyFactory {
 	public static RacingGameStrategy createRandomsStrategy(int min, int max, int threshold) {
 		if (min < 0 || max < 0 || threshold < 0) {
 			throw new IllegalArgumentException("범위 값과 경계 값은 음수일 수 없습니다.");
-		} else if (min > max) {
+		}
+		if (min > max) {
 			throw new IllegalArgumentException("최소값은 최대값보다 클 수 없습니다.");
-		} else if (threshold > max) {
+		}
+		if (threshold > max) {
 			throw new IllegalArgumentException("경계 값은 최대값보다 클 수 없습니다.");
 		}
 		return () -> Randoms.pickNumberInRange(min, max) >= threshold;
